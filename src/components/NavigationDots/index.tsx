a77eec140fab446ca71512cc2b5431c243e752fb
Manoel de Souza Rocha Neto <carvo@pop-os.localdomain>
2022-02-27 20:17:07 -0300

    Adds App wrapper and some updates to other components's styling

---
+++ src/components/NavigationDots/index.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+interface NavigationDotsProps {
+  active: string;
+}
+
+export default function NavigationDots({ active }: NavigationDotsProps) {
+  return (
+    <div className="app__navigation">
+      {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+        <a
+          href={`#${item}`}
+          // eslint-disable-next-line react/no-array-index-key
+          key={item + index}
+          className="app__navigation-dot"
+          style={active === item ? { backgroundColor: '#313BAC' } : { }}
+        />
+      ))}
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as NavBar } from './NavBar';
+export { default as SocialMedia } from './SocialMedia';
+export { default as NavigationDots } from './NavigationDots';
