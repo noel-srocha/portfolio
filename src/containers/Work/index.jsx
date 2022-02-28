3c060a2cfe24d1ad95670515fc2b2242c58e4684
Manoel de Souza Rocha Neto <carvo@pop-os.localdomain>
2022-02-28 12:02:57 -0300

    Adds Skills component and its styling, and Motion Wrapper for containers

--- src/containers/Work/index.jsx
+++ src/containers/Work/index.jsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
 import styles from './styles.module.scss';
@@ -118,4 +118,8 @@ function Work() {
   );
 }
 
-export default AppWrap(Work, 'work');
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  'app__primarybg',
+);
