397b54ec12496f656f7c243896b0412cc18222d8
Manoel de Souza Rocha Neto <56612428+noel-srocha@users.noreply.github.com>
2022-03-02 08:49:08 -0300

    Revert "Main Development Branch"

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import './styles/App.scss';
-
-import {
-  About,
-  Header,
-  Footer,
-  Testimonial,
-  Work,
-  Skills,
-} from './containers';
-
-import {
-  NavBar,
-} from './components';
 
 function App() {
   return (
     <div className="app">
-      <NavBar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Testimonial />
-      <Footer />
+      App
     </div>
   );
 }
--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as NavBar } from './NavBar';
-export { default as SocialMedia } from './SocialMedia';
-export { default as NavigationDots } from './NavigationDots';
